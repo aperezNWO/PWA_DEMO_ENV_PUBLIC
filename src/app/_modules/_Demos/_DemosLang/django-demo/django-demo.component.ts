9e65d448de7703891347f9281f11275c292dbf58
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-03-11 21:40:47 -0500

    Application [ANGULAR DEMO - ENV_PUBLIC] v[1.5.6] 1)  Search Service with params.

--- src/app/_modules/_Demos/_DemosLang/django-demo/django-demo.component.ts
+++ src/app/_modules/_Demos/_DemosLang/django-demo/django-demo.component.ts
@@ -1,3 +1,4 @@
+/*
 import { Component, PipeTransform, QueryList, ViewChildren } from '@angular/core';
 import { Directive, EventEmitter, Input, Output            } from '@angular/core';
 import { DecimalPipe                                       } from '@angular/common';
@@ -306,5 +307,31 @@ export class DjangoDemoComponent {
     this.searchTerm = "";
   }
 }
+*/
 
 
+import { Component                                         } from '@angular/core';
+import { _environment                                      } from 'src/environments/environment';
+import { _BaseModel                                        } from 'src/app/_models/common/entityInfo.model';
+import { _BaseSortEvent                                    } from 'src/app/_headers/sortable.directive';
+import { SearchService                                     } from 'src/app/_services/searchService/search.service';
+import { ENV_LIST_DJANGO_PYTHON_DEMO, PAGE_ID, PAGE_SIZE   } from 'src/app/_models/common/common';
+import { SearchComponent                                   } from 'src/app/_components/search/search.component';
+
+@Component({
+  selector: 'app-django-demo',
+  templateUrl: './django-demo.component.html',
+  styleUrl: './django-demo.component.css',
+  providers: [
+    SearchService,
+    { provide: PAGE_ID,   useValue: ENV_LIST_DJANGO_PYTHON_DEMO }, // Unique ID for this component
+    { provide: PAGE_SIZE, useValue: 8                           } 
+  ]
+})
+export class DjangoDemoComponent extends SearchComponent  
+{
+  constructor(searchService         : SearchService)
+  {
+      super(searchService);
+  }
+} 
