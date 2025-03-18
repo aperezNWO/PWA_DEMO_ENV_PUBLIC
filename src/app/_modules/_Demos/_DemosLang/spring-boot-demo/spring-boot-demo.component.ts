e8913d8a731eba1b6218aa195d03674d90dddfd8
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-03-18 14:10:33 -0500

    Application [ANGULAR DEMO - ENV_PUBLIC] v[1.7.5]. 1) Both ways reference.

--- src/app/_modules/_Demos/_DemosLang/spring-boot-demo/spring-boot-demo.component.ts
+++ src/app/_modules/_Demos/_DemosLang/spring-boot-demo/spring-boot-demo.component.ts
@@ -4,6 +4,7 @@ import { _BaseModel                                        } from 'src/app/_mode
 import { SearchService                                     } from 'src/app/_services/searchService/search.service';
 import { ENV_LIST_SPRING_BOOT_DEMO, PAGE_ID, PAGE_SIZE,SEARCH_TERM     } from 'src/app/_models/common';
 import { SearchComponent                                               } from 'src/app/_components/search/search.component';
+import { ConfigService                                                 } from 'src/app/_services/ConfigService/config.service';
 
 //
 @Component({
@@ -14,7 +15,10 @@ import { SearchComponent                                               } from 's
     SearchService,
     { provide: PAGE_ID,     useValue: ENV_LIST_SPRING_BOOT_DEMO   }, // Unique ID for this component
     { provide: PAGE_SIZE,   useValue: 8                           },
-    { provide: SEARCH_TERM, useValue: ""                          }   
+    { provide: SEARCH_TERM  
+         ,  useFactory: (configService: ConfigService) => configService.queryUrlParams("searchTerm")                         
+         ,  deps: [ConfigService], // Dependencies required by the factory function
+    }   
   ]
 })
 export class SpringBootDemoComponent extends SearchComponent  
