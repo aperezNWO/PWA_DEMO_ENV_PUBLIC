ed6ce2b5126eb8f0090143f0e1f581c3bd20e18d
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-07-20 20:35:32 -0500

    Application [ANGULAR DEMO - ENV_PUBLIC] v[2.0.34]
    1) Fractal Demo. Generate PDF

--- src/app/_modules/_Demos/_DemosFeatures/miscelaneous/fractalDemo/juliaform.component.ts
+++ src/app/_modules/_Demos/_DemosFeatures/miscelaneous/fractalDemo/juliaform.component.ts
@@ -1,7 +1,8 @@
 import { HttpClient                     } from '@angular/common/http';
-import { Component                      } from '@angular/core';
+import { Component, ViewChild                      } from '@angular/core';
 import { ActivatedRoute                 } from '@angular/router';
 import { BaseComponent                  } from 'src/app/_components/base/base.component';
+import { PdfService } from 'src/app/_engines/pdf.engine';
 import { PAGE_MISCELANEOUS_FRACTAL_DEMO } from 'src/app/_models/common';
 import { BackendService                 } from 'src/app/_services/BackendService/backend.service';
 import { ConfigService                  } from 'src/app/_services/ConfigService/config.service';
@@ -21,12 +22,15 @@ export class FractalDemoComponent  extends BaseComponent {
   imageUrl: string | null = null;
   submitTitle : string = "Generate Fractal";
   //
+  @ViewChild('_fractal_image')  _fractal_image  : any;
+  //
   constructor(public          shapeDetectionService   : ShapeDetectionService,
               public override configService           : ConfigService,
               public override backendService          : BackendService,
               public override route                   : ActivatedRoute,
               public override speechService           : SpeechService,
               public http                             : HttpClient,
+              public pdfEngine                        : PdfService,
   )
   {
       //
@@ -36,7 +40,7 @@ export class FractalDemoComponent  extends BaseComponent {
             speechService,
             PAGE_MISCELANEOUS_FRACTAL_DEMO);
   }
-   
+  // 
   onSubmit() {
     //
     const url        = `${this.configService.getConfigValue('baseUrlNetCoreCPPEntry')}generatejuliaparams/?maxIterations=${this.maxIterations}&realPart=${this.realPart}&imagPart=${this.imagPart}`;
@@ -60,5 +64,37 @@ export class FractalDemoComponent  extends BaseComponent {
         this.status_message.set("[Ha ocurrido un error, favor intente de nuevo]");
       }
     );
+  }
+  //
+  GeneratePDF() {
+    //
+    let fileName_input  : string     = `FRACTAL_IMAGE`;
+    let fileName_output : string     = '';
+    //
+    this.status_message.set('Generando PDF');
+    //
+    this.pdfEngine._GetPDF
+      (
+        this.pageTitle,
+        this._fractal_image,
+        this._fractal_image,
+        fileName_input,
+      )
+      .subscribe(
+      {
+        next: (fileName: string) =>{
+          //
+          fileName_output = fileName;
+        },
+        error: (error: { message: string; }) => {
+            //
+            this.status_message.set('ha ocurrido un error : ' + error.message);
+        },
+        complete: () => {
+            //
+            this.status_message.set(`[Se ha generado el archivo PDF]`);
+        }
+      }
+    );      
   }
 }
