72ef2b5c3eb5819a9bc8bfd4a2d11184fed26e36
aperezNWO <alejandro.perez.acosta@hotmail.com>
2025-08-08 21:43:34 -0500

    Application [ANGULAR DEMO - ENV_PUBLIC] v[2.2.0]
    1) CMS components - Grid Param

--- src/app/_components/grid-param/grid-param.component.ts
+++ src/app/_components/grid-param/grid-param.component.ts
@@ -1,10 +1,49 @@
-import { Component } from '@angular/core';
+import { Component                                                     } from '@angular/core';
+import { Router                                                        } from '@angular/router';
+import { _environment                                                  } from 'src/environments/environment';
+import { _BaseModel                                                    } from 'src/app/_models/entity.model';
+import { PAGE_DEMOS_DJANGO_PYTHON, PAGE_ID, PAGE_SIZE,SEARCH_TERM      } from 'src/app/_models/common';
+import { ConfigService                                                 } from 'src/app/_services/ConfigService/config.service';
+import { __SearchComponent                                             } from '../search/__search.component';
+import { __SearchService                                               } from 'src/app/_services/searchService/__search.service';
+
 
 @Component({
   selector: 'app-grid-param',
   templateUrl: './grid-param.component.html',
-  styleUrl: './grid-param.component.css'
+  styleUrl: './grid-param.component.css',
+  providers: [
+    ConfigService,    
+    __SearchService,
+    { provide: PAGE_ID
+         ,  useFactory: (configService: ConfigService) => configService.queryUrlParams("pageName")                         
+         ,  deps: [ConfigService]
+    }, // Dependencies required by the factory function
+    { provide: PAGE_SIZE,   useValue  : 8                          },
+    { provide: SEARCH_TERM  
+         ,  useFactory: (configService: ConfigService) => configService.queryUrlParams("searchTerm")                         
+         ,  deps: [ConfigService], // Dependencies required by the factory function
+    }  
+  ]
 })
-export class GridParamComponent {
+export class GridParamComponent extends __SearchComponent  
+{
+      //
+      toogleLisCaption: string = "[Ir a Referencia - Django / Python ...]";
+      //
+      constructor(
+                  public override searchService         : __SearchService,
+                  public          router                : Router,
+      )
+      {
+          //
+          super(searchService);
+      }
+      //
+      toggleList() 
+      {
+          //
+          this.router.navigateByUrl('/CurriculumDjango'); // Redirects to '/target-route'
+      }
 
-}
+} 
